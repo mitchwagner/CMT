0e719159758a78b2a70f5242404b540b81ee80cd
Faiz Abidi <fabidi89@vt.edu>
2016-11-23 23:03:00 -0500

    Removed headers, and replaces empty values with 0.

--- Tweets_Update_Incremetal_HDFS/adjust_new_lines.bash
+++ Tweets_Update_Incremetal_HDFS/adjust_new_lines.bash
@@ -26,5 +26,16 @@ rm tempfile1.txt
 sed -i 1d $filename
 
 # Add headers to the file
-sed -i.bak '1i\
-archivesource\ttext\tto_user_id\tfrom_user\tid\tfrom_user_id\tiso_language_code\tsource\tprofile_image_url\tgeo_type\tgeo_coordinates_0\tgeo_coordinates_1\tcreated_at\ttime' $filename
+#sed -i.bak '1i\
+#archivesource\ttext\tto_user_id\tfrom_user\tid\tfrom_user_id\tiso_language_code\tsource\t/
+#profile_image_url\tgeo_type\tgeo_coordinates_0\tgeo_coordinates_1\tcreated_at\ttime' $filename
+
+# Replace missing data with 0
+CMD3="awk 'BEGIN { FS = OFS = "\t" } { for(i=1; i<=NF; i++) /
+if($i ~ /^ *$/) $i = 0 }; 1' $filename > tempfile2.txt"
+eval CMD3
+
+# Move interim file to the original filename
+mv tempfile2.txt > $filename
+
+
